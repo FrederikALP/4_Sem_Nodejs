eb9c40425cfea56851404a671c502064df3adab4
FrederikALP <fred05k3@stud.kea.dk>
2022-03-08 13:46:51 +0100

    first svelte project created

--- 3._World_Wide_Express/app.js
+++ 3._World_Wide_Express/app.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const fetch = require("node-fetch");
+
+//use node-fetch
+//create endpoint /proxy
+//fetch from googles homepage
+//response.text()
+//send the page as a response
+
 
 // specifies the static folder for the client, allowing the client to access the files on this path
 // it is a security feature to allow the client only access to certain parts
@@ -19,6 +27,20 @@ app.use(dinosaurrouter.router);  //only uses the endpoint specified in the route
 app.get("/", (req, res) => {
     // __ signifies that it is total global, it is a global variable
     res.sendFile(__dirname + "/public/frontpage/frontpage.html");
+});
+
+//node-fetch is not the same as fetch in browser, they reuse some of the same code but still not quite the same
+app.get("/proxy", (req, res) => {
+    fetch("https://www.google.dk")
+        .then(res => res.text())
+        .then(page => res.send(page));
+});
+
+//async/await method
+app.get("/proxyawait", async (req, res) => {
+    const response = await fetch("https://www.google.dk")
+    const homepage = await response.text();
+        res.send(homepage);
 });
 
 // "" does not allow multiple lines
--- 3._World_Wide_Express/public/frontpage/frontpage.js
+++ 3._World_Wide_Express/public/frontpage/frontpage.js
@@ -1,1 +1,11 @@
 console.log("rrrawr");
+
+
+fetch("/calculatecooldinosaurs")
+.then(response => response.json())
+.then(dinosaurs => {
+    console.log(dinosaurs);
+    const amountdino = document.getElementById("cool-dinosaurs")
+
+    amountdino.innerText = dinosaurs.data;
+});
